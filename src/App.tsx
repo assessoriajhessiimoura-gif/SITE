5483fd63157385adce1451ed885e2f32d579ef39
assessoriajhessiimoura-gif <assessoriajhessiimoura@gmail.com>
2026-02-17 19:16:27 -0300

    Add overflow-x-hidden to main div

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="min-h-screen">
+    <div className="min-h-screen overflow-x-hidden bg-white">
       <Hero />
       <Procedures />
       <Course />
